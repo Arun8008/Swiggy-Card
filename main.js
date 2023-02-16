6ea012c024b323202420bd6fd5a1965fc70936d3
Arun8008 <arun.sivanpandian.2000@gmail.com>
2023-02-16 09:37:06 +0530

    update

---
+++ main.js
@@ -0,0 +1,280 @@
+let footList=[
+		 
+		 {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xtbjvdoy12tk77utojex",
+			 name:"Hotel Saravana Bhavan",
+			 place:"South Indian, North Indian, Sweets",
+			 rating:4.3,
+			 time:"30 MINS",
+			 amount:"₹300 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/7d93ecf49daaec4a521b5d19b59560b9",
+			 name:"Erode Amman Mess",
+			 place:"South Indian, Biryani, North Indian, Chinese",
+			 rating:4.5,
+			 time:"41 MINS",
+			 amount:"₹500 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/o5vlkrbbjez8gbei4utj",
+			 name:"Moonlight Takeaway & Delivery",
+			 place:"Indian, Chinese",
+			 rating:4.1,
+			 time:"30 MINS",
+			 amount:"₹300 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bzykgwhz9pywsgrdijq5",
+			 name:"Hotel Santosi",
+			 place:"North Indian, Chinese",
+			 rating:4.0,
+			 time:"26 MINS",
+			 amount:"₹300 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gqj6v2lcwa88eqinumyj",
+			 name:"Hotel Al Ali",
+			 place:"Biryani, North Indian",
+			 rating:4.6,
+			 time:"29 MINS",
+			 amount:"₹300 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/u3cqtyhaazgkmnej6ytv",
+			 name:"Copper Kitchen",
+			 place:"Biryani, Barbecue, Chettinad, Chinese",
+			 rating:4.2,
+			 time:"32 MINS",
+			 amount:"₹400 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ji1dtwtd368vir8t0pik",
+			 name:"Eating Circles",
+			 place:"South Indian",
+			 rating:4.5,
+			 time:"30 MINS",
+			 amount:"₹200 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/igxyfqbu82muvgsgusca",
+			 name:"Shree Mahaas Veg Restaurant",
+			 place:"North Indian, Chinese",
+			 rating:4.1,
+			 time:"30 MINS",
+			 amount:"₹500 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+			 name:"KFC",
+			 place:"Burgers, Biryani",
+			 rating:4.2,
+			 time:"33 MINS",
+			 amount:"₹400 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hdg9plj56ypwpsfjrdqp",
+			 name:"Madurai Pandiyan Hotel ",
+			 place:"South Indian, Chettinad",
+			 rating:4.5,
+			 time:"45 MINS",
+			 amount:"₹400 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bhq3cgee5sjz9xxw5cks",
+			 name:"Sangeetha Veg Restaurant",
+			 place:"North Indian, Chinese",
+			 rating:4.4,
+			 time:"23 MINS",
+			 amount:"₹200 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ssgdwsduggnczeeybi9x",
+			 name:"Geetham Veg restaurant",
+			 place:"South Indian, North Indian",
+			 rating:4.6,
+			 time:"38 MINS",
+			 amount:"₹400 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/a0fgfd6a5fotxuyv0yvq",
+			 name:"Namma Veedu Vasanta Bhavan",
+			 place:"South Indian",
+			 rating:4.3,
+			 time:"33 MINS",
+			 amount:"₹150 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/i77m4w0qoymx5ouasqzb",
+			 name:"Namma Bhai Biryani",
+			 place:"Biryani, Indian",
+			 rating:4.1,
+			 time:"38 MINS",
+			 amount:"₹250 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/l6wbfanprsmqipixi6fr",
+			 name:"Sri Balajee Bhavan",
+			 place:"South Indian",
+			 rating:4.2,
+			 time:"32 MINS",
+			 amount:"₹250 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		  {
+			 imgUrl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yhaxvuqphmpjtparnqdi",
+			 name:"Chai waale",
+			 place:"Bakery, Beverages",
+			 rating:4.4,
+			 time:"24 MINS",
+			 amount:"₹100 FOR TWO",
+			 offer:"50% off | Use WELCOME50",
+			 quick:"QUICK VIEW",
+		 },
+		 
+		 
+	  
+]
+
+let cont=document.querySelector(".container");
+let cards=document.createElement("div");
+cards.classList.add("row");
+cont.appendChild(cards);
+
+footList.forEach(function(e){
+	let div=document.createElement("div");
+	div.classList.add("col");
+	cards.appendChild(div);
+	div.style.minHeight="550px";
+	
+	
+	let div2=document.createElement("div");
+	div2.classList.add("col-card");
+	div.appendChild(div2);
+	div.style.cursor="pointer";
+	
+	
+	
+	let img=document.createElement("img");
+	img.src=e.imgUrl;
+	div2.appendChild(img);
+	img.style.width="100%";
+	img.style.height="auto";
+	
+	let head=document.createElement("h2");
+	head.innerHTML=e.name;
+	div2.appendChild(head);
+	head.style.fontSize="17px";
+	head.style.margin="10px 0";
+	head.style.fontWeight="500"
+	head.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	
+	let para=document.createElement("p");
+	para.innerHTML=e.place;
+	div2.appendChild(para);
+	para.style.margin="5px 0";
+	para.style.color="#686b78";
+	para.style.padding="2px 0";
+	para.style.fontSize="14px";
+	para.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	
+	let div3=document.createElement("div");
+	div3.classList.add("row");
+	div2.appendChild(div3);
+	
+	let para2=document.createElement("p");
+	para2.innerHTML=`<iconify-icon class="icon" icon="material-symbols:star"></iconify-icon>${e.rating}`;
+	div3.appendChild(para2);
+	para2.style.backgroundColor="#48c479";
+	para2.style.color="#fff";
+	para2.style.padding="3px 8px";
+	para2.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	
+	let dot=document.createElement("p");
+	dot.innerHTML=`.`;
+	div3.appendChild(dot);
+	
+	let para3=document.createElement("p");
+	para3.innerHTML=e.time;
+	div3.appendChild(para3);
+    para3.style.color="#535665";
+	para3.style.fontSize="13px";
+	para3.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	para3.style.padding="8px";
+	
+	let dott=document.createElement("p");
+	dott.innerHTML=`.`;
+	div3.appendChild(dott);
+	
+	let para4=document.createElement("p");
+	para4.innerHTML=e.amount;
+	div3.appendChild(para4);
+	para4.style.color="#535665";
+	para4.style.fontSize="13px";
+	para4.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	para4.style.padding="8px";
+	
+	let para5=document.createElement("p");
+    para5.innerHTML=`<iconify-icon class="icon-2" icon="bxs:offer"></iconify-icon>${e.offer}`;
+	div2.appendChild(para5);
+	para5.style.borderTop="2px solid #e9e9eb";
+	para5.style.padding="10px 0";
+	para5.style.color="#8a584b";
+	para5.style.fontSize="14px";
+	para5.style.fontWeight="500";
+	para5.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+	
+	let quick=document.createElement("a");
+	quick.classList.add("quick-btn");
+	quick.innerHTML=e.quick;
+	div2.appendChild(quick);
+	quick.style.color="#5d8ed5";
+	quick.style.paddingTop="20px";
+	quick.style.fontSize="14px";
+	quick.style.fontWeight="600";
+	quick.style.fontFamily="ProximaNova,arial,Helvetica Neue,sans-serif";
+});
+console.log(cont);
